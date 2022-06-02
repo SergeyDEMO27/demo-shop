bd1399f6ec1e681e576d0f0d5a9b7a513604b9b0
Sergey L <yo-suke@mail.ru>
2022-06-02 20:49:59 +0300

    add scroll to top after route changes

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,8 @@ import FilterPage from '@/components/FilterPage.vue';
 import ProductPage from '@/components/ProductPage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
+const scrollBehavior = () => window.scrollTo(0, 0);
+
 const routes = [
   {
     path: '/',
@@ -22,6 +24,10 @@ const routes = [
   },
 ];
 
-const router = createRouter({ routes, history: createWebHistory(process.env.BASE_URL) });
+const router = createRouter({
+  routes,
+  history: createWebHistory(process.env.BASE_URL),
+  scrollBehavior,
+});
 
 export default router;
